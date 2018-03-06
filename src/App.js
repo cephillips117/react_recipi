e11c429aa638244f6d8e72a6b82a21bf4dc2cfab
Chris Phillips <cephillips117@gmail.com>
2018-03-06 17:33:28 -0500

    test_push
    This adds a comment to the app.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PersistentDrawer from './slideoutDrawer/slideOutDrawer';
 import './App.css';
 
+// This comment is going to master
 class App extends Component {
   render() {
     return (
